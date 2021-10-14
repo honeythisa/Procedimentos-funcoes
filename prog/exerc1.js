35aba168f41f795aa31303b2f7e4374319397e5e
Melissa Medeiros Teodoro <honeythisa@gmail.com>
2021-10-14 16:08:20 -0300

    lista funções
    lista de loops colocado em funções

--- prog/exerc1.js
+++ prog/exerc1.js
@@ -1,61 +1,64 @@
 /* 
-1. Utilizando os exercícios desenvolvidos na lista passada, coloque  a implementação de cada exercício dentro de uma função;
+1. Utilizando os exercícios desenvolvidos na lista passada, coloque a implementação de cada exercício dentro de uma função;
 2. dados de entrada de cada programa via parâmetro
 let resultado = zeroAquinhentos(46)
 console.log(resultado)
-/maneira simplificada: console.log(zeroAquinhentos(46))
+--> maneira simplificada: console.log(zeroAquinhentos(46))
 */
-// exercicío 1
-function zeroAquinhentos(n) {
-    if (n >= 0 && n <= 500) {
-        if (n >= 100 && n <= 200) {
-            return "[VERIFICADO] Número entre 100 e 200."
-        } else {
-            return "[VERIFICADO] mas número fora da faixa."
-        }
-    } else {
-        return "Não estamos verificando esse número."
-    }
+// exercicío 01
+function zeroAquinhentos() {
+  let contador = 0
+  contador++
+  for (contador = 500; contador >= 0; contador--) {
+      if (contador >= 0 && contador < 100) {
+          console.log(contador, "Número na faixa.");
+      } else {
+          console.log(contador, "Número fora da faixa.");
+      }
+  }
 }
-console.log(zeroAquinhentos(100)) //parâmetro real
+console.log(zeroAquinhentos()) // parâmetro real
 
-// exercício 2
-function parImp(nu) {
-    if (nu >= 0 && nu <= 100) {
-        if (nu % 2 == 0) {
-            return "Número par."
-        } else {
-            return "Número ímpar."
-        }
-    }
-    else {
-        return "Número fora da faixa meu chapa."
-    }
+// exercício 02
+function parImp(number) {
+  let contador = 0
+  contador++
+  for (contador = 100; contador >= 0; contador--) { // para imprimir todos os numeros de 100 ate 0
+      if (contador % 2 == 0) { // verificar se o numero é par ou impar
+          console.log(contador, "Número par.")
+      } else {
+          console.log(contador, "Número ímpar.")
+      }
+  }
 }
-console.log(parImp(10)) // parâmetro real
+console.log(parImp()) // parâmetro real
 
-// exercício 3
+// exercício 03
 function fatorial(num) {
-    let fat = 1
-    for (let c = num; c > 1; c--) { // ele vai pegar o valor até chegar no 1.
-        fat *= c
-    } return `${num}! = ${fat}`
+  let fat = 1
+  for (let c = num; c > 1; c--) { // ele vai pegar o valor pra multiplicar até chegar no 1.
+      fat *= c
+  } return `${num}! = ${fat}`
 }
 console.log(fatorial(5)) //parâmetro real
 
-// exercício 4
-function multiplos(nume){
-    let cont = 0
-    while(cont < 20){
-        console.log (`${nume} x ${cont + 1} = ${nume}`)
-        cont++
-    }
+// exercício 04
+function multiplos(nume) {
+  let cont = 0;
+  while (cont < 20) {
+      console.log(`${nume} x ${cont + 1} = ${nume * (cont + 1)}`);
+      cont++;
+  }
 }
-console.log(multiplos(8)) // parâmetro real
+console.log(multiplos(8)) //parâmetro real
 
-// exercício 5
-function agenda(op){
+// exercício 05
+function agenda(op) {
     do {
         console.log("--------- Menu ---------\n| 1 - adicionar contato|\n| 2 - buscar contato   |\n| 3 - editar contato   |\n| 4 - sair             |\n------------------------ ")
-    }while(op)
+    }while(op != 4)
+    return op
 }
+console.log(agenda(4)) //parâmetro real
+
+// exercício 06
